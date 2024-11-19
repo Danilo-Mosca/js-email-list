5f08ef7b51aee2bac787e8bbeaa50a30c1282648
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-19 10:39:02 +0100

    I add the loader when clicking the "Genera email" button

--- js/script.js
+++ js/script.js
@@ -9,12 +9,17 @@ https://flynn.boolean.careers/exercises/api/random/mail
 const btnGenerator = document.getElementById('btnGenerator');
 // Creo un ascoltatore di eventi per il pulsante
 btnGenerator.addEventListener('click', getData);
-// Creo la variabile arrayEmail e la popolo delle 10 email casuali
+
+// Recupero l'elemento loader dalla pagina html
+const loader = document.getElementById('loader');
 
 
 
 /* Funzione che restituirà i dati contenente le 10 email casuali in un array */
 function getData() {
+  // Rimuovo la classe d-none dal loader
+  loader.classList.remove('d-none');
+  
   // Array vuoto che conterra le 10 email generate casualmente
   const arrayEmail = [];
   // Ciclo per 10 volte così da generare le 10 email casuali e inserirle nell'array:
@@ -23,6 +28,10 @@ function getData() {
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) => {
       // controllo se i dati vengono immagazzinati mostrando la risposta sul console.log
       console.log(result.data);
+      console.log(result.data.response);
+      console.log(result.data.success);
+      
+      
       // Inserisco le email nell'array: a noi serve la chiave response dell'oggetto, quindi è quella che assegnerò all'array contenente le email
       arrayEmail.push(result.data.response);
       // Verifico che siamo arrivati tutte i dati delle 10 email 
@@ -49,9 +58,12 @@ function printEmail(arrayEmail) {
   let elementLi = document.createElement('li');
   elementLi.value = '';
   arrayEmail.forEach((element, index) => {
-    console.log(index);
+    // console.log(index);
     
-    elementLi.innerHTML += `<li class="li-email my-3 p-3" id="email-${index + 1}"> <i class="fa-regular fa-envelope"></i> ${element} </li>`
+    elementLi.innerHTML += `<li class="li-email my-3 p-3" id="email-${index + 1}"> <i class="fa-regular fa-envelope"></i> ${element} </li>`;
     elementUl.appendChild(elementLi);
+    
+    // Una volta aggiunti tutti gli elementi riaggiungo la classe d-none al div loader
+    loader.classList.add('d-none');
   });
 }
